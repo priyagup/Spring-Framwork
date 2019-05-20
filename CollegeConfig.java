4dd00c44a5c236dfbc440bc3dec84ef174700369
Priya Gupta <priyagupta14317@gmail.com>
2019-05-19 21:10:51 -0500

    Delete

--- CollegeConfig.java
+++
@@ -1,6 +0,0 @@
-@Configure
-@ComponentScan({com.package.college}) // Or you can write (Basepackage = "com.package.college")
-@PropertySource("classpath: college-info.properties")
-public class CollegeConfig{
-
-}
